96d1ec91dfd30b32ac78be5f9c7022131f77d75c
Kaleb Davenport <kaleb@theearlynerd.org>
2022-01-07 15:27:13 -0500

    Update expo version

--- src/library/index.ts
+++ src/library/index.ts
@@ -1,4 +1,2 @@
-export { default as Button } from './Atoms/Buttons/BaseButton';
+export { default as Button } from "./Atoms/Buttons/BaseButton";
 export { default as Text } from "./Atoms/Text/Text";
-
-
--- src/library/theme.ts
+++ src/library/theme.ts
@@ -1,33 +1,74 @@
 import { createTheme } from "@shopify/restyle";
 
 const palette = {
-  yellow: "#FFCC00",
-  lightYellow: "#e9d499",
-  lightOrange: "#E7C69B",
-  orange: "#E57D1F",
-  darkOrange: "#E58950",
-  red: "#CA2B3B",
-  lightRed: "#f1bbc0",
-  green: "#17A165",
-  lightGreen: "#c9dbbc",
-  brightGreen: "#25de8e",
-  darkGreen: "#138855",
-  purple: "#7F5289",
-  brown: "#3a2e04",
-  lightBrown: "#605d51",
-  darkBrown: "#241C0D",
-  blue: "#0864A3",
-  lightBlue: "#7a92a5",
-  backgroundBlue: "#a6ceee",
+  sunglowYellow: "#FFCC00",
+  mustardYellow: "#FFD633",
+  crayolaYellow: "#FFEB99",
+  tangerineOrange: "#F18A13",
+  yelorannge: "#F9A94E",
+  lightPeach: "#FFDCAD",
+  crimsonRed: "#C92238",
+  amaranth: "#DD4055",
+  mauvelous: "#EF9FAA",
+  airforceBlue: "#16559C",
+  brightNavy: "#1F74D6",
+  tuftsBlue: "#4C94E6",
+  darkOliveBrown: "#48390A",
+  bisterBrown: "#7E6411",
+  lemonCurryBrown: "#C9A326",
+  darkSpringGreen: "#127D46",
+  pantoneGreen: "#1CB55C",
+  aquamarine: "#74E7AA",
+  teaGreen: "#DBFBDF",
+  greenGlow: "#F4FBF4",
 
-  black: "#0B0B0B",
-  white: "#F0F2F3",
+  black: "#191710",
+
+  darkGrey: "#3D444D",
+  slateGrey: "#738487",
+  mediumGrey: "#B3CCC8",
+  mintGrey: "#DBE6E1",
+  lightMintGrey: "#F5F5F5",
+
+  offWhite: "#F8FCFA",
+  white: "#FFFFFF",
 };
 
 const theme = createTheme({
   colors: {
-    primary: palette.green,
+    brandActionLight: palette.darkSpringGreen,
+    brandAction: palette.pantoneGreen,
+    brandActionDark: palette.aquamarine,
+
+    brandInactionLight: palette.lemonCurryBrown,
+    brandInaction: palette.bisterBrown,
+    brandInactionDark: palette.darkOliveBrown,
+
+    questionLight: palette.lightPeach,
+    question: palette.yelorannge,
+    questionDark: palette.tangerineOrange,
+
+    answerLight: palette.tuftsBlue,
+    answer: palette.brightNavy,
+    answerDark: palette.airforceBlue,
+
+    dangerLight: palette.mauvelous,
+    danger: palette.amaranth,
+    dangerDark: palette.crimsonRed,
+
+    urgentLight: palette.crayolaYellow,
+    urgent: palette.mustardYellow,
+    urgentDark: palette.sunglowYellow,
+
     black: palette.black,
+
+    darkGrey: palette.darkGrey,
+    slateGrey: palette.slateGrey,
+    mediumGrey: palette.mediumGrey,
+    mintGrey: palette.mintGrey,
+    lightMintGrey: palette.lightMintGrey,
+
+    offWhite: palette.offWhite,
     white: palette.white,
   },
   spacing: {
